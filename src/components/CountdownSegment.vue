f0979359a7648b751ac3688f643e2e22e631e730
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2022-12-03 17:54:48 +0000

    solution completed with date-fns

--- src/components/CountdownSegment.vue
+++ src/components/CountdownSegment.vue
@@ -7,7 +7,10 @@ defineProps({
 <template>
   <div class="segment text-center">
     <div class="pt-10 overflow-hidden relative">
-      <span :key="number" class="numbers text-green absolute top-0 left-[50%]">{{ number }}</span>
+      <transition name="slide-up">
+
+        <span :key="number" class="numbers text-green absolute top-0 left-[50%]">{{ number }}</span>
+      </transition>
     </div>
 
     <span class="label block pt-2">{{ label }}</span>
@@ -17,11 +20,28 @@ defineProps({
 .segment {
   width: 80px;
 }
+
 .numbers {
   transform: translateX(-50%);
   font-size: 32px;
 }
+
 .label {
   font-size: 16px;
+}
+
+.slide-up-enter-active,
+.slide-up-leave-active {
+  transition: all 0.5s ease;
+}
+
+.slide-up-enter-from {
+  opacity: 0;
+  transform: translateX(-50%) translateY(100%);
+}
+
+.slide-up-leave-to {
+  opacity: 0;
+  transform: translate(-50%) translateY(-100%);
 }
 </style>
